f190f3466be6060f73c65be6755276a97e409061
Bryce Jensen <50688409+brycewynton@users.noreply.github.com>
2021-04-13 17:51:10 -0500

    compiles and passes tests, contract has mint functionality

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -2,10 +2,47 @@ pragma solidity 0.5.0;
 
 import "./ERC721Full.sol";
 
-
+/**
+ * smoothbrain We were able to import the ERC721Full contracts
+ * from this local directory by using truffle-flattener
+ * Not recommended for in-production builds, but is used often...
+ * 
+ * 
+ *
+ */
 contract Color is ERC721Full {
+	string[] public colors;
+	// global variable colors is an array of strings
+	// we will be able to access dif colors via indices
+	mapping(string => bool) _colorExists;
 	
-	constructor () ERC721Full("Color", "COLOR") public {
+	constructor() ERC721Full("Color", "COLOR") public { 
+
+	}
+
+	/**
+	 * dev mint privelege should be restricted to owner.
+	 * See more on minter roles on openzeppelin
+	 *
+	 * param string _color 
+	 * Must be a unique local variable.
+	 *
+	 */
+
+	function mint(string memory _color) public {
+		/**
+		 * 1. require unique color, no duplicates
+		 * 2. Color - add it
+		 * 3. Call the mint function
+		 * 4. Color - track it
+		 */
+
+		 uint _id = colors.push(_color);
+		 // assigns an id for every color minted
+		 _mint(msg.sender, _id);
+		 // calls internal mint function to msg.sender
+		 _colorExists[_color] = true;
+
 
 
 	}
--- test/Color.test.js
+++ test/Color.test.js
@@ -12,6 +12,7 @@ contract('Color', (accounts) => {
 	})
 
 	describe('deployment', async () => {
+
 		it('deploys successfully', async () => {
 			const address = contract.address
 			console.log(address)
@@ -19,7 +20,6 @@ contract('Color', (accounts) => {
 			assert.notEqual(address, '')
 			assert.notEqual(address, null)
 			assert.notEqual(address, undefined)
-
 		})
 
 		it('has a name', async () => {
@@ -30,6 +30,45 @@ contract('Color', (accounts) => {
 		it('has a symbol', async () => {
 			const symbol = await contract.symbol()
 			assert.equal(symbol, 'COLOR')
+		})
+	})
+
+	describe('minting', async () => {
+
+		it('creates a new token', async () => {
+
+			const result = await contract.mint('#EC058E')
+			const totalSupply = await contract.totalSupply()
+			// SUCCESS:
+			assert.equal(totalSupply, 1)
+			const event = result.logs[0].args
+			assert.equal(event.tokenId.toNumber(), 1, 'id is correct')
+			assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct')
+			assert.equal(event.to, accounts[0], 'to is correctd')
+
+			// FAILURE:
+			await contract.mint('#EC058E').should.be.rejected;
+		})
+	})
+
+	describe('indexing', async () => {
+		it('lists colors', async => {
+			//mint 3 more tokens
+			await contract.mint('#5386E4')
+			await contract.mint('#FFFFFF')
+			await contract.mint('#000000')
+			const totalSupply = await.contract.totalSupply()
+
+			let color
+			let result = []
+
+			for (var i = i; i <= totalSupply; i++) {
+				color = await contract.colors(i - 1)
+				result.push(color)
+			}
+
+			let expected = ['#EC058E', '#5386E4', '#FFFFFF', '#000000']
+			assert.equal(result.join(','), expected.join(','))
 		})
 	})
 
